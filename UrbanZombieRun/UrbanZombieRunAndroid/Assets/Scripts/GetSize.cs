b8093bc19329823782b6df8a867e79cf4c65c759
bladematthewslp <matthewstimothym@gmail.com>
2015-05-25 10:08:29 -0500

    backing up urban zombie run

---
+++ UrbanZombieRun/UrbanZombieRunAndroid/Assets/Scripts/GetSize.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetSize : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		double s = GetComponent<Collider>().bounds.size.z;
+		Debug.Log( s);
+		double d = GetComponent<Renderer>().bounds.size.z;
+		Debug.Log(d);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ UrbanZombieRun/UrbanZombieRunAndroid/Assets/Scripts/PlayerCameraScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCameraScript : MonoBehaviour 
+{
+	public float distance 	= 10;
+	public Camera camera;
+	public Vector3 cameraOffset;
+	public GameObject objectToLookAt;
+	public Vector3 offset = new Vector3(0,-2, 5);
+	// Use this for initialization
+	void Start () 
+	{
+
+		cameraOffset.z = distance;
+		camera.transform.position = objectToLookAt.transform.position + cameraOffset;
+		camera.transform.LookAt (objectToLookAt.transform.position + offset );
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		camera.transform.position = objectToLookAt.transform.position + cameraOffset;
+		camera.transform.LookAt (transform.position + offset);
+	}
+}
